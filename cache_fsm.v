af3d4f77933f8fa61c2178058cc2a46d07f1b503
Omie2806 <omgupta2806@gmail.com>
2025-10-07 07:38:01 +0000

    Update cache_fsm.v

--- cache_fsm.v
+++ cache_fsm.v
@@ -13,7 +13,7 @@ module cache_fsm #(
     input  logic [DATA_WIDTH - 1 : 0]   cache_data_in,cache_data_in_from_main_mem,
     output logic [DATA_WIDTH - 1 : 0]   cache_data_out,cache_data_out_to_main_mem,
     output logic                        data_ready,write_to_main_mem,
-    output logic [DATA_WIDTH - 1 : 0]   data_out_to_main_mem,data_out
+    output logic [DATA_WIDTH - 1 : 0]   data_out
 );
 
 localparam ADD           = ADDRESS_WIDTH;
@@ -25,25 +25,25 @@ wire [TAG_WIDTH - 1 : 0]    tag_match    = mem_add[ADD - 1 : TAG_IN_ADD];       
 wire [SET_WIDTH - 1 : 0]    set_match    = mem_add[TAG_IN_ADD - 1 : SET_IN_ADD]; //set separation
 wire [OFFSET_WIDTH - 1 : 0] offset_match = mem_add[SET_IN_ADD - 1 : 0];          //offset separation
 
-typedef enum logic [2:0] { idle, cache_hit, cache_dirty, cache_not_dirty } state_type;
+typedef enum logic [2:0] { idle, cache_dirty, cache_not_dirty } state_type;
 state_type state_curr, state_next;
 
 logic                        cpu_req, cache_h;
 logic                        mem_ready;
-logic                        mem_not_dirty, mem_dirty;
+logic                        mem_dirty;
 
 always @(posedge clk , posedge reset) begin
     if(reset) begin
-        state_curr           <= idle;
-        mem_ready            <= 1'b0;
-        mem_dirty            <= 1'b0;
-        cpu_req              <= 1'b0;
-        cache_h              <= 1'b0;
-        cache_data_out       <= 'b0;
-        data_ready           <= 1'b0;
-        write_to_main_mem    <= 1'b0;
-        data_out_to_main_mem <= 'b0;
-        data_out             <= 'b0;
+        state_curr                 <= idle;
+        mem_ready                  <= 1'b0;
+        mem_dirty                  <= 1'b0;
+        cpu_req                    <= 1'b0;
+        cache_h                    <= 1'b0;
+        cache_data_out             <= 'b0;
+        data_ready                 <= 1'b0;
+        write_to_main_mem          <= 1'b0;
+        cache_data_out_to_main_mem <= 'b0;
+        data_out                   <= 'b0;
     end
     else begin
         state_curr   <= state_next;
@@ -52,8 +52,12 @@ always @(posedge clk , posedge reset) begin
 end
 
 always @(*) begin
-    state_next = state_curr;
-    
+    state_next                 = state_curr;
+    cache_data_out             = 'b0;
+    cache_data_out_to_main_mem = 'b0;
+    data_ready                 = 1'b0;
+    write_to_main_mem          = 1'b0;
+    data_out                   = 'b0;     
     case (state_curr)
         idle: begin
             if(cpu_req == 1'b1) begin
@@ -85,6 +89,7 @@ always @(*) begin
             //write into cache from main memory 
             data_out   = cache_data_in_from_main_mem;
             data_ready = 1'b1;
+            mem_dirty  = 1'b0;
             if(~mem_ready) begin
                 state_next = cache_not_dirty;
             end
